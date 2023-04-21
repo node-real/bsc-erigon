6a14c67b2d8bd3cd4ab91b33d5a983f7ad8d77d1
Alex Sharov <AskAlexSharov@gmail.com>
2023-04-21 09:51:03 +0700

    e3: remove garbage files (#7357)

--- eth/stagedsync/stage_snapshots.go
+++ eth/stagedsync/stage_snapshots.go
@@ -164,6 +164,8 @@ func DownloadAndIndexSnapshotsIfNeed(s *StageState, ctx context.Context, tx kv.R
 	}
 
 	if cfg.historyV3 {
+		cfg.agg.CleanDir()
+
 		indexWorkers := estimate.IndexSnapshot.Workers()
 		if err := cfg.agg.BuildMissedIndices(ctx, indexWorkers); err != nil {
 			return err
