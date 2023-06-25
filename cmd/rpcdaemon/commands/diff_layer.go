d5750f88918dbbf28368505d50677891a830e337
avatarH <avatar.h@nodereal.io>
2023-06-25 15:38:50 +0800

    rlp encode to json marshal

--- cmd/rpcdaemon/commands/diff_layer.go
+++ cmd/rpcdaemon/commands/diff_layer.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/holiman/uint256"
 	libcommon "github.com/ledgerwatch/erigon-lib/common"
@@ -219,6 +220,6 @@ func (dlw *DiffLayerWriter) GetData() []byte {
 		}
 	}
 
-	data, _ := rlp.EncodeToBytes(dlw.layer)
+	data, _ := json.Marshal(dlw.layer)
 	return data
 }
