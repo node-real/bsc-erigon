87fea2fcf3f8f63c2eac4433e5e83efb29960b66
blxdyx <blxdyx@outlook.com>
2024-09-26 11:34:00 +0800

    fix eth_getTransactionReceipt

--- core/state_transition.go
+++ core/state_transition.go
@@ -337,7 +337,7 @@ func (st *StateTransition) TransitionDb(refunds bool, gasBailout bool) (*evmtype
 	// BSC always gave gas bailout due to system transactions that set 2^256/2 gas limit and
 	// So when trace systemTx, skip PreCheck
 	var skipCheck bool
-	if st.isParlia && st.msg.Gas() == math.MaxUint64/2 && st.gasPrice == uint256.NewInt(0) {
+	if st.isParlia && st.msg.Gas() == math.MaxUint64/2 && st.gasPrice.IsZero() {
 		skipCheck = true
 	}
 
