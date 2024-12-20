d3f904ebfa81a9119b6923d745bced138ca8d54b
blxdyx <blxdyx@outlook.com>
2024-12-20 11:19:00 +0800

    upstream alpha7

--- core/state_transition.go
+++ core/state_transition.go
@@ -365,10 +365,6 @@ func (st *StateTransition) TransitionDb(refunds bool, gasBailout bool) (*Executi
 
 	// BSC always gave gas bailout due to system transactions that set 2^256/2 gas limit and
 	// for Parlia consensus this flag should be always be set
-	if st.isParlia {
-		gasBailout = true
-	}
-
 	var skipCheck bool
 	if st.evm.ChainConfig().Parlia != nil && st.msg.Gas() == math.MaxUint64/2 && st.msg.From() == coinbase {
 		skipCheck = true
