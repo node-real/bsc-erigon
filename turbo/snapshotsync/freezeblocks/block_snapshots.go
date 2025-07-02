258a5b3a04c4d3c7923141b904d06773df9747b9
blxdyx <blxdyx@users.noreply.github.com>
2025-07-02 20:55:55 +0800

    snapshots: merge to bigger snapshots

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -517,6 +517,12 @@ func (br *BlockRetire) RemoveOverlaps() error {
 		}
 	}
 
+	if br.chainConfig.Parlia != nil {
+		if err := br.bscSnapshots().RoSnapshots.RemoveOverlaps(); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -525,6 +531,9 @@ func (br *BlockRetire) MadvNormal() *BlockRetire {
 	if br.chainConfig.Bor != nil {
 		br.borSnapshots().RoSnapshots.MadvNormal()
 	}
+	if br.chainConfig.Parlia != nil {
+		br.bscSnapshots().RoSnapshots.MadvNormal()
+	}
 	return br
 }
 
@@ -532,6 +541,9 @@ func (br *BlockRetire) DisableReadAhead() {
 	br.snapshots().DisableReadAhead()
 	if br.chainConfig.Bor != nil {
 		br.borSnapshots().RoSnapshots.DisableReadAhead()
+	}
+	if br.chainConfig.Parlia != nil {
+		br.bscSnapshots().RoSnapshots.DisableReadAhead()
 	}
 }
 
