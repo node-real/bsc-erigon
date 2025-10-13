a852bf2c3179cd58747e340f5162e3b6c69fd0cb
blxdyx <blxdyx@users.noreply.github.com>
2025-10-13 14:55:27 +0800

    snapshot.go: fix ci

--- execution/consensus/parlia/snapshot.go
+++ execution/consensus/parlia/snapshot.go
@@ -353,7 +353,7 @@ func (s *Snapshot) apply(headers []*types.Header, chain consensus.ChainHeaderRea
 			}
 		}
 		snap.RecentForkHashes[number] = hex.EncodeToString(header.Extra[extraVanity-nextForkHashSize : extraVanity])
-		if chainConfig.IsFermi(header.Number, header.Time) {
+		if chainConfig.IsFermi(header.Number.Uint64(), header.Time) {
 			snap.BlockInterval = params2.FermiBlockInterval
 		} else if chainConfig.IsMaxwell(header.Number.Uint64(), header.Time) {
 			snap.BlockInterval = params2.MaxwellBlockInterval
